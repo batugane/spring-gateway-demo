8e7e4c8f1f78969a8dd1949089c1a86f074d0c88
batuhan.duras <batugane@gmail.com>
2024-04-16 09:55:05 +0300

    feat: CircuitBreaker implementation

--- src/main/java/dev/duras/gateway/GatewayApplication.java
+++ src/main/java/dev/duras/gateway/GatewayApplication.java
@@ -9,18 +9,22 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80"))
-				.build();
-	}
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+                        .uri("http://httpbin.org:80")).
+                build();
+    }
 
 }
